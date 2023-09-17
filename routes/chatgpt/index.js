43194f7150858309820add4f13e15090440395d5
realmojo <strikers1999@naver.com>
2023-09-18 01:42:26 +0900

    fdfd

--- routes/chatgpt/index.js
+++ routes/chatgpt/index.js
@@ -4,6 +4,9 @@ const gptController = require("./gpt");
 const naverController = require("./naver");
 
 router.get("/naver/link", naverController.getNaverTopNewsLink);
+router.get("/social", naverController.getSocialCrawl);
+router.get("/asia", naverController.getAsiaWorldCrawl);
+router.get("/america", naverController.getAmericaWorldCrawl);
 router.get("/content", gptController.getNewsContent);
 router.get("/keyword", gptController.getKeyword);
 module.exports = router;
--- routes/chatgpt/naver.js
+++ routes/chatgpt/naver.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const getNaverTopNewsLink = async (req, res, next) => {
+const getNaverTopNewsLink = async (req, res) => {
   try {
     const { keyword } = req.query;
     if (!keyword) {
@@ -24,13 +24,101 @@ const getNaverTopNewsLink = async (req, res, next) => {
     return res.status(200).send({ status: "ok", links });
   } catch (e) {
     console.log(e);
-    return next({
-      status: "error",
-      message: e.message,
-    });
+    return res.status(500).send({ status: "err", message: e.message });
+  }
+};
+
+const getSocialCrawl = async (req, res) => {
+  try {
+    const url =
+      "https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=102&sid2=249";
+    const response = await axios(url);
+    const $ = cheerio.load(response.data);
+    const items = $(".type06_headline > li > dl > dt > a");
+
+    const links = [];
+    for (let item of items) {
+      const link = item.attribs.href;
+      if (links.indexOf(link) === -1) {
+        links.push(link);
+      }
+    }
+
+    for (const link of links) {
+      // console.log(link);
+      await axios(
+        `http://localhost:3001/api/chatgpt/content?category=social&url=${link}`
+      );
+    }
+
+    return res.status(200).send({ status: "ok", links });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send({ status: "err", message: e.message });
+  }
+};
+
+const getAsiaWorldCrawl = async (req, res) => {
+  try {
+    const url =
+      "https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=104&sid2=231";
+    const response = await axios(url);
+    const $ = cheerio.load(response.data);
+    const items = $(".type06_headline > li > dl > dt > a");
+
+    const links = [];
+    for (let item of items) {
+      const link = item.attribs.href;
+      if (links.indexOf(link) === -1) {
+        links.push(link);
+      }
+    }
+
+    for (const link of links) {
+      await axios(
+        `http://localhost:3001/api/chatgpt/content?category=world&url=${link}`
+      );
+    }
+
+    return res.status(200).send({ status: "ok", links });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send({ status: "err", message: e.message });
+  }
+};
+
+const getAmericaWorldCrawl = async (req, res) => {
+  try {
+    const url =
+      "https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=104&sid2=232";
+    const response = await axios(url);
+    const $ = cheerio.load(response.data);
+    const items = $(".type06_headline > li > dl > dt > a");
+
+    const links = [];
+    for (let item of items) {
+      const link = item.attribs.href;
+      if (links.indexOf(link) === -1) {
+        links.push(link);
+      }
+    }
+
+    for (const link of links) {
+      await axios(
+        `http://localhost:3001/api/chatgpt/content?category=world&url=${link}`
+      );
+    }
+
+    return res.status(200).send({ status: "ok", links });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send({ status: "err", message: e.message });
   }
 };
 
 module.exports = {
   getNaverTopNewsLink,
+  getSocialCrawl,
+  getAsiaWorldCrawl,
+  getAmericaWorldCrawl,
 };
