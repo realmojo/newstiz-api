05edfe18442bef10bee9220d3c798ec0c2b5bb5d
mk.jung01 <mk.jung01@gsretail.com>
2023-09-25 17:18:29 +0900

    cron

--- routes/cron/cron.js
+++ routes/cron/cron.js
@@ -1,5 +1,13 @@
+const BASE_URL = "https://newstiz-api.vercel.app";
+const axios = require("axios");
+
 const doCron = async (req, res) => {
   try {
+    axios(`${BASE_URL}/api/chatgpt/social`);
+    axios(`${BASE_URL}/api/chatgpt/economy_investing`);
+    axios(`${BASE_URL}/api/chatgpt/life_health`);
+    axios(`${BASE_URL}/api/chatgpt/economy_crypto`);
+    axios(`${BASE_URL}/api/chatgpt/america`);
     return res.status(200).send({ status: "ok" });
   } catch (e) {
     return res.status(500).send({ status: "err", message: e.message });
