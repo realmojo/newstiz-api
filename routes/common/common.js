db9195541ab2caeece85ca0fb88929ffc6523f9b
mk.jung01 <mk.jung01@gsretail.com>
2023-10-05 17:33:37 +0900

    Update common.js

--- routes/common/common.js
+++ routes/common/common.js
@@ -1,11 +1,17 @@
 const os = require("os");
 const ip = require("ip");
+const moment = require("moment");
 
 const getPing = async (req, res) => {
   try {
     return res
       .status(200)
-      .send({ status: "ok", os: os.hostname(), ip: ip.address() });
+      .send({
+        status: "ok",
+        os: os.hostname(),
+        ip: ip.address(),
+        date: moment().format("YYYY-MM-DD HH:mm"),
+      });
   } catch (e) {
     return res.status(500).send({ status: "err", message: e.message });
   }
